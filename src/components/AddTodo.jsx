caa8c90b20f6984fb08e8725ce903bc765e9d0c1
Manav948 <valanimanav61@gmail.com>
2025-03-29 19:55:01 +0530

    fixed AddTodo File

--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -15,13 +15,13 @@ function AddTodo() {
             <input
                 type="text"
                 placeholder="Enter a task..."
-                className="flex-grow border border-gray-900 rounded-lg px-3 py-2 outline-none shadow-sm focus:ring-2 focus:ring-blue-500"
+                className="flex-grow border border-gray-300 rounded-lg px-3 py-2 outline-none shadow-sm focus:ring-2 focus:ring-blue-500"
                 value={input}
                 onChange={(e) => setInput(e.target.value)}
             />
             <button
                 type="submit"
-                className="bg-blue-900 text-white px-4 py-2 rounded-lg hover:bg-blue-700 transition-all shadow-md"
+                className="bg-blue-600 text-white px-4 py-2 rounded-lg hover:bg-blue-700 transition-all shadow-md"
             >
                 Add
             </button>
